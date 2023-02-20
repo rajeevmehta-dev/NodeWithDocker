cd608f4c60af73215880ab5ef547c751e9eb9a24
VeeJar <65169394+rajeevmehta-dev@users.noreply.github.com>
2023-02-20 14:40:46 +0530

    Updating the fiile
    Updating the fiile

--- route.js
+++ route.js
@@ -4,7 +4,7 @@ router.get('/api', (req, res, next) => {
     const userData = [
         {
             id: 101,
-            name: 'Shaktiman Ok',
+            name: 'Shaktiman Ok Ok',
             designation: 'Software Engineer Shaktimaan'
         },
         {
